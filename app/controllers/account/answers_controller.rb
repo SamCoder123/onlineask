e51072a5c5ecda92cf26895828cd49748f397f3c
chenyuli6 <18301662790@163.com>
2016-08-16 14:57:10 +0800

    account/questions&answers

---
+++ app/controllers/account/answers_controller.rb
@@ -0,0 +1,52 @@
+class Account::AnswersController < ApplicationController
+  before_action :set_answer, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @answers = Answer.all
+  end
+
+  def show
+  end
+
+  def new
+    @answer = Answer.new
+  end
+
+  def edit
+  end
+
+  def create
+    @answer = Answer.new(answer_params)
+
+    if @answer.save
+      redirect_to account_answers_path, notice: '回答已发送！'
+    else
+      render :new
+    end
+
+  end
+
+  def update
+    if @answer.update(answer_params)
+      redirect_to account_answers_path, notice: '回答已更新！'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @answer.destroy
+    redirect_to account_answers_path, notice: '回答已删除！'
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_answer
+      @answer = Answer.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def answer_params
+      params.require(:answer).permit(:content, :is_hidden, :user_id, :question_id)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :answers
   resources :questions
   devise_for :users
+
   root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
@@ -19,4 +20,10 @@ Rails.application.routes.draw do
       end
     end
   end
+
+  namespace :account do
+    resources :answers
+    resources :questions
+  end
+
 end
