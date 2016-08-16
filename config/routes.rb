4e4c2ef25102825598af5a429b8f3737168ac735
chenyuli6 <18301662790@163.com>
2016-08-16 14:57:10 +0800

    account/questions&answers

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,11 @@ Rails.application.routes.draw do
   resources :answers
   resources :questions
   devise_for :users
+
+  namespace :account do
+    resources :answers
+    resources :questions
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 end
