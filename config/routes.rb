69761f737a23e0c1ac6f85115a7242de045b716e
liusining <siningdream@gmail.com>
2016-08-16 15:31:17 +0800

    add admin/questions&answers#index, #show, #hide, #publish

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,19 @@ Rails.application.routes.draw do
   resources :questions
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :admin do
+    resources :questions do
+      member do
+        post :hide
+        post :publish
+      end
+    end
+    resources :answers do
+      member do
+        post :hide
+        post :publish
+      end
+    end
+  end
 end
