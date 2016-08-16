d8334a2166991d72f2ff8a2b7f1a4527f6d1ccec
Jane <jane.dujuan.hr@gmail.com>
2016-08-16 15:52:11 +0800

    add profile

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,17 @@ Rails.application.routes.draw do
   resources :questions
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # root "registrations#new"
+
+  namespace :account do
+    resources :users do
+      member do
+        get :new_profile
+        get :edit_profile
+        put :update_profile
+        get :show_profile
+      end
+    end
+  end
+
 end
