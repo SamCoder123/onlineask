9165d85ebdf3254dddda0b498bd5e4ad653c1e81
chenyuli6 <18301662790@163.com>
2016-08-16 14:16:39 +0800

    question's and answer's CRUD

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,6 @@
 class QuestionsController < ApplicationController
   before_action :set_question, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!,only:[:create]
 
   # GET /questions
   # GET /questions.json
@@ -26,39 +27,53 @@ class QuestionsController < ApplicationController
   def create
     @question = Question.new(question_params)
 
-    respond_to do |format|
-      if @question.save
-        format.html { redirect_to @question, notice: 'Question was successfully created.' }
-        format.json { render :show, status: :created, location: @question }
-      else
-        format.html { render :new }
-        format.json { render json: @question.errors, status: :unprocessable_entity }
-      end
+    if @question.save
+      redirect_to @question, notice: '提问成功！'
+    else
+      render :new
     end
+
+    # respond_to do |format|
+    #   if @question.save
+    #     format.html { redirect_to @question, notice: 'Question was successfully created.' }
+    #     format.json { render :show, status: :created, location: @question }
+    #   else
+    #     format.html { render :new }
+    #     format.json { render json: @question.errors, status: :unprocessable_entity }
+    #   end
+    # end
   end
 
   # PATCH/PUT /questions/1
   # PATCH/PUT /questions/1.json
   def update
-    respond_to do |format|
-      if @question.update(question_params)
-        format.html { redirect_to @question, notice: 'Question was successfully updated.' }
-        format.json { render :show, status: :ok, location: @question }
-      else
-        format.html { render :edit }
-        format.json { render json: @question.errors, status: :unprocessable_entity }
-      end
+    if @question.update(question_params)
+      redirect_to @question, notice: '提问修改成功！'
+    else
+      render :edit
     end
+
+    # respond_to do |format|
+    #   if @question.update(question_params)
+    #     format.html { redirect_to @question, notice: 'Question was successfully updated.' }
+    #     format.json { render :show, status: :ok, location: @question }
+    #   else
+    #     format.html { render :edit }
+    #     format.json { render json: @question.errors, status: :unprocessable_entity }
+    #   end
+    # end
   end
 
   # DELETE /questions/1
   # DELETE /questions/1.json
   def destroy
     @question.destroy
-    respond_to do |format|
-      format.html { redirect_to questions_url, notice: 'Question was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+
+    redirect_to questions_url, notice: '提问成功删除！'
+    # respond_to do |format|
+    #   format.html { redirect_to questions_url, notice: 'Question was successfully destroyed.' }
+    #   format.json { head :no_content }
+    # end
   end
 
   private
