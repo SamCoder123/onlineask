05b1496bb6fcc4299fa3d8cd17b3fd5527144910
Jane <jane.dujuan.hr@gmail.com>
2016-08-16 12:00:00 +0800

    add addtional columns to table users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160816025724) do
+ActiveRecord::Schema.define(version: 20160816034611) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -25,6 +25,13 @@ ActiveRecord::Schema.define(version: 20160816025724) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "role"
+    t.boolean  "is_admin"
+    t.text     "description"
+    t.string   "gender"
+    t.string   "school"
+    t.string   "major"
+    t.string   "image"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
