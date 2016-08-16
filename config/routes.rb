7ab292d8f32d032b2aff918e4385c58a53abb169
zmlfdogw <chinamichaelzhang@192.168.2.2>
2016-08-16 13:56:34 +0800

    admin_user complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :admin do
+    resources :users do
+      member do
+        post :change_to_admin
+        post :change_to_user
+      end
+    end
+  end
 end
