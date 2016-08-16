9f687b7c07a8b9d1a6c660e16f283b46aba5b245
liumiao2023 <liumiao2023@hotmail.com>
2016-08-16 19:05:27 +0800

    re-design front-page

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -8,13 +8,13 @@ class Admin::UsersController < ApplicationController
 
   def change_to_admin
     @user = User.find(params[:id])
-    @user.change_to_admin
+    @user.change_to_admin!
     redirect_to :back
   end
 
   def change_to_user
     @user = User.find(params[:id])
-    @user.change_to_user
+    @user.change_to_user!
     redirect_to :back
   end
 
