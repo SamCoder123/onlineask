ec51aae755dfa6d2bfcdeefbcbede5b99eca0b3b
xdite <xuite.joke@gmail.com>
2016-08-17 20:13:03 +0800

    update schema

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -5,3 +5,16 @@ class Answer < ApplicationRecord
   scope :published,-> {where(is_hidden:false)}
 
 end
+
+# == Schema Information
+#
+# Table name: answers
+#
+#  id          :integer          not null, primary key
+#  content     :text
+#  is_hidden   :boolean
+#  user_id     :integer
+#  question_id :integer
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
