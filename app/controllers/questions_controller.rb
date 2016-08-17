0a1336beb4946de78c13ed061195b0a3a7b930f7
chenyuli6 <18301662790@163.com>
2016-08-17 12:32:31 +0800

    add downpayment

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -28,7 +28,7 @@ class QuestionsController < ApplicationController
     @question = Question.new(question_params)
     @question.user = current_user
     if @question.save
-      redirect_to questions_path, notice: '提问成功！'
+      redirect_to root_path, notice: '提问成功！'
     else
       render :new
     end
@@ -84,6 +84,6 @@ class QuestionsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def question_params
-      params.require(:question).permit(:title, :description, :vote, :is_hidden, :user_id)
+      params.require(:question).permit(:title, :description, :downpayment)
     end
 end
