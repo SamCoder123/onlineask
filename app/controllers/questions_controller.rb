e824e9656bf771df621af2255e1ec6efabe175a9
xdite <xuite.joke@gmail.com>
2016-08-18 15:45:37 +0800

    add cont

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,7 +1,7 @@
 class QuestionsController < ApplicationController
   before_action :set_question, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user!, only: [ :create, :new, :edit, :update, :destroy]
-  before_filter :validate_search_key , only: [:search]
+  before_action :authenticate_user!, only: [:create, :new, :edit, :update, :destroy]
+  before_filter :validate_search_key, only: [:search]
 
   def index
     @questions = Question.all
@@ -45,27 +45,26 @@ class QuestionsController < ApplicationController
 
   def search
     if @query_string.present?
-      #binding.pry
+      # binding.pry
       search_result = Question.ransack(@search_criteria).result(distinct: true)
-      @questions = search_result.paginate(page: params[:page], per_page:20)
-      #set_page_title "搜寻 #{@query_string}"
+      @questions = search_result.paginate(page: params[:page], per_page: 20)
+      # set_page_title "搜寻 #{@query_string}"
     end
   end
 
   protected
 
   def validate_search_key
-
-    @query_string = params[:query_string].gsub(/\\|\'|\/|\?/, "") if params[:query_string].present?
+    @query_string = params[:query_string].gsub(/\\|\'|\/|\?/, '') if params[:query_string].present?
     @search_criteria = search_criteria(@query_string)
   end
 
-
   def search_criteria(query_string)
-    { title_or_description: query_string }
+    { title_or_description_cont: query_string }
   end
 
   private
+
   # Use callbacks to share common setup or constraints between actions.
   def set_question
     @question = Question.find(params[:id])
