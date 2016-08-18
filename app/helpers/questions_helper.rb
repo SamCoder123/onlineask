b8f808da8aa9f8b845f2a447238944049f01345a
chenyuli6 <18301662790@163.com>
2016-08-18 19:49:23 +0800

    modify

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,6 +1,5 @@
 module QuestionsHelper
   def render_highlight_content(question,query_string)
-    # binding.pry
     excerpt_cont = excerpt(question.title, query_string, radius: 500)
     highlight(excerpt_cont, query_string)
   end
