26f04339da0ebfd1231878fb59f328a41ddf91c9
chenyuli6 <18301662790@163.com>
2016-08-23 11:19:09 +0800

    fix some bug and optimize some code

--- app/helpers/account/questions_helper.rb
+++ app/helpers/account/questions_helper.rb
@@ -1,14 +1,8 @@
 module Account::QuestionsHelper
   def account_question_status(status)
     question_status = {open:'开放',closed:'关闭'}
-    question_status[status.to_sym]
-  end
-
-  def show_button_base_status(question,answer)
-    content_tag(:div) do
-      if question.status=="open"
-        concat link_to('赏他', to_downpayment_account_question_path(@question,answer_id:answer.id),method: :post,class:"btn btn-xs btn-danger",data: { confirm: "确定要赏他么?" })
-      end
+    unless status.nil?
+      question_status[status.to_sym]
     end
   end
 
