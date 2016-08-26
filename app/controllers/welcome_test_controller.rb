074b77ae4eb880affe2feee160ed39a2ece5eac5
chenyuli6 <18301662790@163.com>
2016-08-26 15:03:03 +0800

    fix bug

--- app/controllers/welcome_test_controller.rb
+++ app/controllers/welcome_test_controller.rb
@@ -4,8 +4,13 @@ class WelcomeTestController < ApplicationController
   def index
     # @questions = Question.all
     @questions = Question.limit(6)
-    if current_user
-      redirect_to show_profile_account_user_path(current_user)
+
+    if request.referrer.include?('/users/sign_in') || request.referrer.include?('/users/sign_up')
+      if current_user.admin?
+        redirect_to admin_admins_path
+      else
+        redirect_to show_profile_account_user_path(current_user)
+      end
     end
   end
 end
