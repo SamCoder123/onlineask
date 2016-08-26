b30e83b3af793a71ee33347866d585920d52c0dc
liusining <siningdream@gmail.com>
2016-08-26 09:21:37 +0800

    index to admin/admins

--- app/controllers/welcome_test_controller.rb
+++ app/controllers/welcome_test_controller.rb
@@ -2,10 +2,15 @@ class WelcomeTestController < ApplicationController
   layout "welcome_test"
 
   def index
-    # @questions = Question.all
-    @questions = Question.limit(6)
+    if current_user && current_user.is_admin
+      redirect_to admin_admins_path
+      return
+    end
     if current_user
       redirect_to show_profile_account_user_path(current_user)
+      return
     end
+    # @questions = Question.all
+    @questions = Question.limit(6)
   end
 end
