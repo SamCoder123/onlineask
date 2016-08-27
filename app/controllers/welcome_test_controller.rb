3f2720495e55786bcd279e5ee9e75ac63664134b
chenyuli6 <18301662790@163.com>
2016-08-27 11:49:11 +0800

    fix bug

--- app/controllers/welcome_test_controller.rb
+++ app/controllers/welcome_test_controller.rb
@@ -5,7 +5,16 @@ class WelcomeTestController < ApplicationController
     # @questions = Question.all
     @questions = Question.limit(6)
 
-    if request.referrer.include?('/users/sign_in') || request.referrer.include?('/users/sign_up')
+    #记录上一次请求路径是否是登录或者注册
+    flag = false
+    unless request.referrer==nil
+      if request.referrer.include?('/users/sign_in') || request.referrer.include?('/users/sign_up')
+        flag = true
+      end
+    end
+
+    # 如果上次请求是登录或注册，直接跳入个人首页。
+    if flag
       if current_user.admin?
         redirect_to admin_admins_path
       else
