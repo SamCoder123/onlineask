53e2a9a8acc4163d2e42404b6481c93623bed910
liusining <siningdream@gmail.com>
2016-08-28 22:05:15 +0800

    display bill in admin

--- app/controllers/admin/admins_controller.rb
+++ app/controllers/admin/admins_controller.rb
@@ -7,11 +7,22 @@ class Admin::AdminsController < ApplicationController
     @questions = Question.all
     @answers = Answer.all
     @users = User.all
+    @bills = Bill.all
     drop_breadcrumb("后台首页")
   end
 
-  def questions_bill
+  def bill
     @open_questions = Question.where(status: "open")
     @closed_questions = Question.where(status: "closed")
+    @bills = case params[:order_method]
+      when 'flow_in'
+        Bill.where(flow: "in") #多个筛选条件怎么搞？
+      when 'flow_out'
+        Bill.where(flow: "out")
+      when 'question_deposit'
+        Bill.where(detail: "提问押金")
+      else
+        Bill.all
+      end
   end
 end
