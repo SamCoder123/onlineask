822381ab4935947ce22524f467b6bb3b84060fec
DJ <jane.dujuan.hr@gmail.com>
2016-09-07 20:11:28 +0800

    add judge if paid reward

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -3,4 +3,14 @@ module QuestionsHelper
     excerpt_cont = excerpt(question.title, query_string, radius: 500)
     highlight(excerpt_cont, query_string)
   end
+
+  def render_question_closed?(question)
+    if question.status == "open"
+
+      content_tag(:span, "悬赏金额 ¥ #{question.downpayment}", class:"media-meta pull-left", style: "padding-right:20px")
+    else
+      label_tag "已悬赏"
+    end
+  end
+
 end
