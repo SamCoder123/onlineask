852e49cef9ca2e91e20bd253e05842229776e771
liusining <siningdream@gmail.com>
2016-09-08 13:44:54 +0800

    counter_cache built

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -45,12 +45,13 @@ end
 #
 # Table name: answers
 #
-#  id            :integer          not null, primary key
-#  content       :text
-#  is_hidden     :boolean          default(FALSE)
-#  user_id       :integer
-#  question_id   :integer
-#  created_at    :datetime         not null
-#  updated_at    :datetime         not null
-#  answer_status :string           default("pending")
+#  id                         :integer          not null, primary key
+#  content                    :text
+#  is_hidden                  :boolean          default(FALSE)
+#  user_id                    :integer
+#  question_id                :integer
+#  created_at                 :datetime         not null
+#  updated_at                 :datetime         not null
+#  answer_status              :string           default("pending")
+#  answer_subscriptions_count :integer
 #
