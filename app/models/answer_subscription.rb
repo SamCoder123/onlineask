b0115712da7e63b8d394b481651775bce20bfca7
liusining <siningdream@gmail.com>
2016-09-13 13:51:41 +0800

    Liusining 0913 seo replyers (#200)
    * change payment style
    
    * configure exhibition_q_a
    
    * tab personal questions and answers
    
    * tab invitated_questions
    
    * add partial pop up
    
    * change gravator
    
    * add sort in index
    
    * add created on and updated on
    
    * add judge if paid reward
    
    * find css
    
    * clean controller
    
    * invitated v1
    
    * commit
    
    * save
    
    * change datetime to date
    
    * move label_tag
    
    * add image_20
    
    * change format of question plaza
    
    * change format
    
    * controller
    
    * add tab_id
    
    * change blog
    
    * img url to exhibition
    
    * prepare counter_cache
    
    * counter_cache built
    
    * change my subs in exihibition
    
    * if current_user
    
    * if nil display
    
    * change my quesitons
    
    * change my answers
    
    * like_answers count
    
    * search bar
    
    * add pay subscription
    
    * db
    
    * ignore counter cache
    
    * render replyers in outside
    
    * public replyers routes
    
    * build replyers data in controller
    
    * auth current_user? in replyers to enable render
    
    * change render
    
    * test icon

--- app/models/answer_subscription.rb
+++ app/models/answer_subscription.rb
@@ -8,11 +8,12 @@ end
 #
 # Table name: answer_subscriptions
 #
-#  id         :integer          not null, primary key
-#  answer_id  :integer
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
-#  user_id    :integer
+#  id             :integer          not null, primary key
+#  answer_id      :integer
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#  user_id        :integer
+#  payment_method :string
 #
 # Indexes
 #
