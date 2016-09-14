be03fa3ab4fceeb89eb2aa9bccea98453abdaf58
liusining <siningdream@gmail.com>
2016-09-14 14:28:18 +0800

    Liusining 0914 backend speedup (#211)
    * confilct
    
    * add guest user
    
    * skip current_user
    
    * skip user_id

--- app/controllers/account_controller.rb
+++ app/controllers/account_controller.rb
@@ -3,11 +3,12 @@ class AccountController < ApplicationController
   before_action :authenticate_user!
 
   def public_replyers_redirect
-    @user = User.find(params[:id])
+
     flag = request.referer && request.referer.include?("/questions/public_replyers")
 
     # 跳转外层show_replyer页面
     if flag
+      @user = User.find(params[:id])
       redirect_to show_replyer_questions_path(user_id:@user)
       return
     end
